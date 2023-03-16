d47cd36be7a287495210aefc38732ffb73ea682d
Jaswinder Singh <jaswindervirk.jv.js@gmail.com>
2023-03-16 19:35:20 -0400

    completing hero section

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -18,13 +18,13 @@ export default function BackgroundCircles({}: Props) {
     className='relative flex justify-center items-center'
     >
         <div className='absolute border border-[#333333] rounded-full h-[200px] w-[200px] 
-        mt-52 animate-ping'></div>
+        mt-72 animate-ping'></div>
         {/* <div className='rounded-full border border-[#333333] h-[300px] w-[300px] absolute 
         mt-52'></div> */}
         <div className='rounded-full border border-[#333333] h-[500px] w-[500px] absolute 
-        mt-52'></div> 
+        mt-72'></div> 
         <div className='rounded-full border border-[#F7AB0A] opacity-20 h-[500px] w-[500px] absolute 
-        mt-52 animate-pulse'></div>
+        mt-72 animate-pulse'></div>
         {/* <div className='rounded-full  border border-[#333333] h-[800px] w-[800px] 
         absolute mt-52'></div> */}
     </motion.div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,6 +4,7 @@ import mypic from '../assets/mypic.jpg'
 
 import { Cursor,useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundCircles';
+import Link from 'next/link';
 type Props = {}
 
 export default function Hero({}: Props) {
@@ -14,19 +15,34 @@ export default function Hero({}: Props) {
     });
   return (
     <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
-        <BackgroundCircles/>
-        <Image className='relative rounded-full h-32 w-32 mx-auto object-cover'
-              src={mypic}
-              width={400}
-              height={400}
-              alt=""
-            />
-        <div>
-        <h1>
-            <span>{text}</span>
+      <BackgroundCircles/>
+      <Image className='relative rounded-full h-32 w-32 mx-auto object-cover'
+        src={mypic}
+        width={400}
+        height={400}
+        alt=""
+       />
+        <div className='z-20'>
+          <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer</h2>
+          <h1 className='text-xl lg:text-3xl font-semibold px-10'>
+            <span className='mr-3'>{text}</span>
             <Cursor cursorColor='#F7AB0A' />
-        </h1>
+          </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className='heroButton'>About</button>
+          </Link>
+          <Link href="#experience">
+            <button className='heroButton'>Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className='heroButton'>Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className='heroButton'>Projects</button>
+          </Link>
         </div>
+      </div>
     </div>
   )
 }
