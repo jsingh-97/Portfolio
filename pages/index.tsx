d60fa34b438376a7c335a6a1c62a2f9a6f62f0c8
Jaswinder Singh <jaswindervirk.jv.js@gmail.com>
2023-03-26 17:05:13 -0400

    Adding projects section

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import Hero from '../components/Hero';
 import About from '../components/About';
 import WorkExperience from '../components/WorkExperience'
 import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import ContactMe from '../components/ContactMe'
+
 export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
@@ -22,6 +25,12 @@ export default function Home() {
       </section>
       <section id="skills" className="snap-start">
         <Skills />
+      </section>
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
+      <section id="section" className='snap-end'>
+        <ContactMe />
       </section>
     </div>
   );
